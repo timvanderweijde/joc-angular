1275ba28cb667f1c0696e18654858e331caa3624
Robin Roestenburg <robinroestenburg@gmail.com>
2013-02-22 17:46:27 +0100

    Start of assignment 2

--- app/js/controllers/coders-controller.js
+++ app/js/controllers/coders-controller.js
@@ -2,5 +2,16 @@
 
 coderContest.controller('CodersController', function($scope) {
 
+  $scope.coders = [
+    { name: "Martin Eich",   votes: 0 },
+    { name: "Martin Fowler", votes: 0 },
+    { name: "Kent Beck",     votes: 0 },
+    { name: "Steve Wozniak", votes: 0 }
+  ];
+
+  $scope.vote = function(coder) {
+    var index = $scope.coders.indexOf(coder);
+    $scope.coders[index].votes += 1;
+  };
 
 });
