3c9f0286f51ab31dac92f60b39fe2187a2b1db64
Robin Roestenburg <robinroestenburg@gmail.com>
2013-02-22 20:21:09 +0100

    Changed styling, added styles for highlighting scopes

--- app/js/coder-contest.js
+++ app/js/coder-contest.js
@@ -1,13 +1,3 @@
 'use strict';
 
-var coderContest = angular.module('coderContest', ['ngResource']);
-
-coderContest.config(['$routeProvider', function($routeProvider) {
-
-  $routeProvider.when('/home',    { templateUrl: 'partials/home.html' });
-  $routeProvider.when('/about',   { templateUrl: 'partials/about.html' });
-  $routeProvider.when('/coders',  { templateUrl: 'partials/coders.html' });
-
-  $routeProvider.otherwise({redirectTo: '/home'});
-
-}]);
+var coderContest = angular.module('coderContest', []);
