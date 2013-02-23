56c54e5b0acebcef3245f2317335ef80d025bae6
Robin Roestenburg <robinroestenburg@gmail.com>
2013-02-23 14:16:46 +0100

    Start of assignment 4

--- app/js/coder-contest.js
+++ app/js/coder-contest.js
@@ -1,3 +1,12 @@
 'use strict';
 
 var coderContest = angular.module('coderContest', []);
+
+coderContest.config(['$routeProvider', function($routeProvider) {
+
+  $routeProvider.when('/home',  { templateUrl: 'partials/coders.html' });
+  $routeProvider.when('/about', { templateUrl: 'partials/about.html' });
+
+  $routeProvider.otherwise({redirectTo: '/home'});
+
+}]);
