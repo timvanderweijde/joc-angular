268dea4ff9c0612ea3b574915290576101bc6efb
Robin Roestenburg <robinroestenburg@gmail.com>
2013-02-25 20:54:11 +0100

    Switched order of assignments, removed routing code.

--- app/js/coder-contest.js
+++ app/js/coder-contest.js
@@ -1,14 +1,3 @@
 'use strict';
 
 var coderContest = angular.module('coderContest', []);
-
-coderContest.config(['$routeProvider', function($routeProvider) {
-
-    $routeProvider.when('/home',    { templateUrl: 'partials/home.html' });
-    $routeProvider.when('/coders',   { templateUrl: 'partials/coders.html' });
-    $routeProvider.when('/coders/:coderId',  { templateUrl: 'partials/coder.html' });
-    $routeProvider.when('/about',  { templateUrl: 'partials/about.html' });
-
-    $routeProvider.otherwise({redirectTo: '/home'});
-
-}]);
