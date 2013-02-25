5b7cdee4ee26624f3dc5f64444bf73eabbc65746
Robin Roestenburg <robinroestenburg@gmail.com>
2013-02-25 20:17:39 +0100

    Add array of coders to controller.

--- app/js/controllers/coders-controller.js
+++ app/js/controllers/coders-controller.js
@@ -2,5 +2,11 @@
 
 coderContest.controller('CodersController', function($scope) {
 
+  $scope.coders = [
+    { name: "Martin Eich",   votes: 0 },
+    { name: "Martin Fowler", votes: 0 },
+    { name: "Kent Beck",     votes: 0 },
+    { name: "Steve Wozniak", votes: 0 }
+  ];
 
 });
